0ddbe97079bd9b455f8c57e11c681107701fa730
juravellar <julliaravellar@gmail.com>
2025-05-18 13:32:42 -0300

    Update AuthFilter.java

--- src/main/java/api/config/AuthFilter.java
+++ src/main/java/api/config/AuthFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @WebFilter(urlPatterns = {"/telaInicialAdmin.jsp"})
 public class AuthFilter implements Filter {
@@ -27,8 +29,9 @@ public class AuthFilter implements Filter {
         }
 
         if (!tipo.equals("admin")) {
-            // Logado, mas não é admin, acesso negado
-            response.sendRedirect("acessoNegado.jsp");
+            String mensagemErro = "Sem permissão para acessar esta página.";
+            String erroEncode = URLEncoder.encode(mensagemErro, StandardCharsets.UTF_8.toString());
+            response.sendRedirect("login.jsp?erro=" + erroEncode);
             return;
         }
 
